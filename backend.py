d68b7ca0d83e6d0d3a4fc7119aa7c3eda96e9272
jobeyes506 <liujian@jobeyes.com>
2025-02-14 14:50:03 +0800

    Add files via upload

--- backend.py
+++ backend.py
@@ -46,4 +46,7 @@ def get_results():
         print(f"❌ API 执行失败: {e}")
         return jsonify({"error": str(e)}), 500
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000, debug=True)
+
 
